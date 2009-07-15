eabb1dd9a8bd6913aad05933de6d94c522c17f10
arm2arm <arm2arm@06a51f3c-712f-11de-a0a7-fb2681086dbc>
2009-07-15 11:27:52 +0000

    Init version , catalogues done but not dumped.

---
+++ trunk/mst_graph/MSTGroup.h
@@ -0,0 +1,132 @@
+#ifndef _MSTGROUP_
+#define _MSTGROUP_
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+
+////////////////////CGalaxy class ////////
+class CMSTGroup{
+public:
+	CMSTGroup(){
+		ResetAll();		
+		};
+	CMSTGroup(int comp, typeVecData *datain):data(datain){
+		ResetAll();
+		ID=comp;
+		}
+	void ResetAll()
+		{
+		
+		memset(com,0,sizeof(com));
+		memset(wcom,0,sizeof(wcom));
+		memset(vel,0,sizeof(vel));
+		memset(wvel,0,sizeof(wvel));
+		memset(npart,0,sizeof(npart));
+		memset(Rxyz,0,sizeof(Rxyz));
+		Ntotal=0;R90=0;R50=0;m_Rxx=0;
+		wtotal=0.0;
+		}
+	void CompileMyProperties(float *pPOS, float *pVEL, float *pW, int np)
+		{
+		for(int i=0,pc=0;i<Ntotal;i++)
+			{
+			pc=id[i]*3;
+			double w=pW[id[i]]*pW[id[i]];
+			for(int j=0;j<3;j++)
+				{
+				com[j]+=pPOS[pc+j];
+				wcom[j]+=(pPOS[pc+j]*w);
+				vel[j]+=(pVEL[pc+j]);
+				wvel[j]+=(pVEL[pc+j]*w);
+				}
+			wtotal+=w;
+			}
+		for(int j=0;j<3;j++)
+			{
+			com[j]/=double(Ntotal);
+			wcom[j]/=(double(Ntotal)*wtotal);
+			vel[j]/=double(Ntotal);
+			wvel[j]/=(double(Ntotal)*wtotal);
+			}
+		};
+	~CMSTGroup(){};
+
+ friend std::ostream& operator<<(std::ostream& os, const CMSTGroup& g)
+    {
+      os.width(1);
+  	  os<<g.ID;
+      os.width(8);
+      os<<" "<<g.Ntotal;
+      
+	  os.precision(4);
+      os.width(10);
+      os.setf( std::ios::fixed, std::ios::floatfield ) ;
+      os<<" "<<g.com[0];
+      
+	  os.width(10);
+      os<<" "<<g.com[1];
+      
+	  os.width(10);
+      os<<" "<<g.com[2];
+
+      os.width(10);			
+      os<<" "<<g.wcom[0];
+      os<<" "<<g.wcom[1];
+      os<<" "<<g.wcom[2];
+
+	  os.width(10);
+      os<<" "<<g.wvel[0];
+      os<<" "<<g.wvel[1];
+      os<<" "<<g.wvel[2];
+
+
+	  os<<std::endl;
+
+      return os;
+    }
+ void Insert(int i){
+	 id.push_back(i);
+	
+	 double w=(*data)[i].w;
+	 w*=w;
+	 for(int j=0;j<3;j++)
+		 {
+		 com[j]+=(*data)[i].pos[j];
+		 wcom[j]+=((*data)[i].pos[j]*w);
+		 vel[j]+=((*data)[i].vel[j]);
+		 wvel[j]+=((*data)[i].vel[j]*w);
+		 }
+	 wtotal+=w;
+
+	 }
+ void DoneInsert()
+	 {
+	 Ntotal=id.size();
+	 for(int j=0;j<3;j++)
+		 {
+		 com[j]/=double(Ntotal);
+		 wcom[j]/=(wtotal*Ntotal);
+		 vel[j]/=double(Ntotal);
+		 wvel[j]/=(wtotal*Ntotal);
+		 }
+	
+	 }
+	/////////////////////////////	
+	vector<int> id;
+	typeVecData *data;
+	double com[3], wcom[3], vel[3],wvel[3];
+	float R90, R50;
+	float m_Rxx;
+	int ID;
+	double wtotal;
+	int npart[3];
+	int Rxyz[3];
+	int Ntotal;
+	};
+
+
+#endif
+
+
---
+++ trunk/mst_graph/mst_graph.h
@@ -0,0 +1,112 @@
+#ifndef _MST_GRAPH_
+#define _MST_GRAPH_
+
+
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/timer.hpp>
+#include <boost/bind.hpp>
+#include <boost/foreach.hpp>
+#include <boost/multi_array.hpp>
+
+#include <boost/graph/subgraph.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/kruskal_min_spanning_tree.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/iteration_macros.hpp>
+#include <boost/graph/connected_components.hpp>
+#include <boost/graph/filtered_graph.hpp>
+#include <boost/math/distributions.hpp>
+//#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/error_of.hpp>
+#include <boost/accumulators/statistics/error_of_mean.hpp>
+//#include <boost/accumulators/statistics.hpp>
+//#include <boost/accumulators/statistics/tail_quantile.hpp>
+
+//using namespace boost::accumulators;
+//typedef accumulator_set<double, stats<boost::accumulators::tag::tail_quantile<left> > > accumulator_t_left;
+//typedef accumulator_set<double, stats<boost::accumulators::tag::tail_quantile<right> > > accumulator_t_right;
+
+using namespace boost;
+using namespace boost::math::policies;
+using namespace boost::math;
+
+
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+#include <numeric>
+#include <functional>
+#include <iostream>
+#include "cycle.h"
+
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+
+typedef unsigned int uint;
+typedef double MyFloat;
+
+template<class T>
+void DumpVector(std::string filename, std::vector<T> vec, unsigned int n=0,bool verbose=false)
+	{
+	std::ofstream of(filename.c_str());
+	if(!of.good())assert("Error");
+	unsigned int i=0, np=min(n,(unsigned int )vec.size());
+	for(i=0;i<np;i++)
+		{
+		if(verbose)cout<<std::setprecision(2)<<std::fixed<<i<<") "<<vec[i]<<endl;
+		of<<i<<" "<<vec[i]<<endl;
+		}
+	of.close();
+	}
+template<class T>
+void DumpVector2(string filename, std::vector<T> vec,std::vector<T> vec1, unsigned int n=0,bool verbose=false)
+	{
+	std::ofstream of(filename.c_str());
+	if(!of.good())assert("Error");
+	unsigned int i=0, np=min(n,(unsigned int )vec.size());
+	for(i=0;i<np;i++)
+		{
+		if(verbose)cout<<std::setprecision(2)<<std::fixed<<i<<") "<<vec[i]<<endl;
+		of<<i<<" "<<vec[i]<<" "<<vec1[i]<<endl;
+		}
+	of.close();
+	}
+
+///////////////////////////////
+
+class scoped_timer {
+	boost::posix_time::ptime start_;
+	std::string m_text;
+public:    
+	inline  void   SetText(std::string text){m_text=text;};
+	scoped_timer(std::string text) 
+		: start_(boost::posix_time::microsec_clock::universal_time()),m_text(text)
+		{
+		m_start= getticks();
+		}
+	~scoped_timer() {
+
+		boost::posix_time::ptime stop( boost::posix_time::microsec_clock::universal_time() );
+		m_end=getticks();
+		double clock_cycles=elapsed(m_end, m_start);
+		std::cout<<std::setprecision(2)<<std::fixed;
+		std::cout <<" "<<m_text<< " done in " << ( stop - start_).total_milliseconds() << " milli seconds or "<<clock_cycles<<" CPU cycles "<<std::endl;
+		}
+protected:
+	ticks m_start;
+	ticks m_end;
+	};
+
+
+#endif
