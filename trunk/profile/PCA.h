9cdf65b74d670030ffdb2df35349086d967f2149
arm2arm <arm2arm@06a51f3c-712f-11de-a0a7-fb2681086dbc>
2010-05-17 19:36:54 +0000

    ping pong

--- trunk/profile/PCA.h
+++ trunk/profile/PCA.h
@@ -14,6 +14,26 @@ class CPCA
 		void eigen (vector<vector<double> > &a, vector<double> &eigen, vector<vector<double> > &eigenvec);
 void GetPCAXYZ(vector<double> &x, vector<double> &y, vector<double> &z);
 inline double Phi(){return m_Phi;};
+ void dump(std::string file="eigendata.log")
+   {
+       std::string fname=file;
+       ofstream of(fname.c_str());
+       if(of.is_open())
+	 {
+	   of<<std::setw(10)<<std::setprecision(7);
+	   copy (eigenvals.begin()+1, eigenvals.end(),
+		   ostream_iterator<double>(of,"\t"));
+	   copy (eigenvec[1].begin()+1, eigenvec[1].end(),
+		   ostream_iterator<double>(of,"\t"));
+	   copy (eigenvec[2].begin()+1, eigenvec[2].end(),
+		 ostream_iterator<double>(of,"\t"));
+	   copy (eigenvec[3].begin()+1, eigenvec[3].end(),
+		 ostream_iterator<double>(of,"\t"));
+	   
+	   of<<endl;
+	 }
+       of.close();
+   } ;
 	private:
 		vector<double>  eigenvals;
 		std::vector<std::vector<double> > eigenvec;
